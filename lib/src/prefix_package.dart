eece2d8092ce1fde06d963489ffa8d10a717f8cc
monfadev <mfaauzii@gmail.com>
2023-12-28 15:19:38 +0700

    test: implementation unit testing

--- lib/src/prefix_package.dart
+++ lib/src/prefix_package.dart
@@ -1,56 +1,55 @@
 ///
 /// [PREFIX PROVIDER PACKAGE]
 ///
-abstract class PrefixProvider {
-  /// LIST OF OPERATOR WITH DATA
-  static List<Operator> data = [
-    Operator(
-      name: "telkomsel",
-      value: ["0812", "0813", "0821", "0822", "0852", "0853", "0823", "0851"],
-    ),
-    Operator(
-      name: "indosat",
-      value: ["0814", "0815", "0816", "0855", "0856", "0857", "0858"],
-    ),
-    Operator(
-      name: "tri",
-      value: ["0895", "0896", "0897", "0898", "0899"],
-    ),
-    Operator(
-      name: "smartfren",
-      value: [
-        "0881",
-        "0882",
-        "0883",
-        "0884",
-        "0885",
-        "0886",
-        "0887",
-        "0888",
-        "0889"
-      ],
-    ),
-    Operator(
-      name: "xl",
-      value: ["0817", "0818", "0819", "0859", "0877", "0878"],
-    ),
-    Operator(
-      name: "axis",
-      value: ["0838", "0831", "0832", "0833"],
-    ),
-    Operator(
-      name: "bolt",
-      value: ["0999", "0998"],
-    ),
-  ];
-}
+
+/// LIST OF OPERATOR WITH DATA
+List<Operator> datas = [
+  Operator(
+    name: "telkomsel",
+    value: ["0812", "0813", "0821", "0822", "0852", "0853", "0823", "0851"],
+  ),
+  Operator(
+    name: "indosat",
+    value: ["0814", "0815", "0816", "0855", "0856", "0857", "0858"],
+  ),
+  Operator(
+    name: "tri",
+    value: ["0895", "0896", "0897", "0898", "0899"],
+  ),
+  Operator(
+    name: "smartfren",
+    value: [
+      "0881",
+      "0882",
+      "0883",
+      "0884",
+      "0885",
+      "0886",
+      "0887",
+      "0888",
+      "0889"
+    ],
+  ),
+  Operator(
+    name: "xl",
+    value: ["0817", "0818", "0819", "0859", "0877", "0878"],
+  ),
+  Operator(
+    name: "axis",
+    value: ["0838", "0831", "0832", "0833"],
+  ),
+  Operator(
+    name: "bolt",
+    value: ["0999", "0998"],
+  ),
+];
 
 /// METHOD PARSE OPERATOR
 String? parseOperator(int? phone) {
   if (phone == null) {
   } else if (phone.toString().length < 3) {
   } else {
-    for (Operator item in PrefixProvider.data) {
+    for (Operator item in datas) {
       if (item.value!.contains("0$phone".toString().substring(0, 4))) {
         return item.name;
       }
---
+++ test/prefix_package_test.dart
@@ -0,0 +1,81 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:prefix_provider/prefix_provider.dart';
+
+void main() {
+  group('Prefix Package Test\n', () {
+    test('Returns list of data telkomsel', () {
+      final telkomselOperator = Operator(
+        name: "telkomsel",
+        value: ["0812", "0813", "0821", "0822", "0852", "0853", "0823", "0851"],
+      );
+      expect(datas[0].name, contains(telkomselOperator.name));
+    });
+
+    test('Constructor initializes correctly', () {
+      final datas = Operator(name: 'telkomsel', value: [
+        "0812",
+        "0813",
+        "0821",
+        "0822",
+        "0852",
+        "0853",
+        "0823",
+        "0851"
+      ]);
+
+      expect(datas.name, 'telkomsel');
+      expect(datas.value,
+          ["0812", "0813", "0821", "0822", "0852", "0853", "0823", "0851"]);
+    });
+
+    test('Constructor with null values initializes correctly', () {
+      final data = Operator();
+
+      expect(data.name, isNull);
+      expect(data.value, isNull);
+    });
+
+    test('Returns correct operator name when valid input is provided', () {
+      const phone = 1234567890;
+
+      datas = [
+        Operator(name: 'Operator1', value: ['0123', '4567']),
+        Operator(name: 'Operator2', value: ['9876', '5432']),
+      ];
+
+      final result = parseOperator(phone);
+
+      expect(result, 'Operator1');
+    });
+
+    test('Returns null when phone is null', () {
+      const phone = null;
+
+      final result = parseOperator(phone);
+
+      expect(result, isNull);
+    });
+
+    test('Returns null when phone length is less than 3', () {
+      const phone = 12;
+
+      final result = parseOperator(phone);
+
+      expect(result, isNull);
+    });
+
+    test('Data is not empty', () {
+      expect(datas, isNotEmpty);
+    });
+
+    test('Returns null data', () {
+      expect(datas = [], isEmpty);
+    });
+
+    test('Data does not contain unknown operator', () {
+      final unknownOperator = Operator(name: "unknown", value: ["1234"]);
+
+      expect(datas, isNot(contains(unknownOperator)));
+    });
+  });
+}
